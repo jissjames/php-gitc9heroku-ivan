9d3877ab3c48873b149f375408fc78e465d39c0b
jissjames <113381566@umail.ucc.ie>
2017-09-29 16:04:51 +0000

    Test

---
+++ TestPHP2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>TestPHP2</title>
+    </head>
+    
+    <body>
+        
+        <?php
+            echo "Welcome ".$_POST["name"]."<br/>";
+            echo "You are ".$_POST["age"]." years old";
+        ?>
+        
+    </body>
+</html>
